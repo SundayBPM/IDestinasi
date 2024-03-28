c38f3c7f554382317568ffa84e3c4b63185615b6
Nazwa Tazkia Kirana <>
2024-03-28 15:59:03 +0700

    ID-25 Menyelesaikan Navigation Bar sebelum login

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+
+@extends('layouts.main')
+
+@section('container')
+   <h1> halaman landing page </h1>
+   <h1> tes </h1>
+@endsection
