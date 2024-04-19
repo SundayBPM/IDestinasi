d7fbf0382b3f0435e5d8790cb016da7c2addccdd
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-04-19 20:48:27 +0700

    ID-45 commit

---
+++ resources/views/informasi_objek_wisata.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Informasi Objek Wisata iDestinasi</title>
+  	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/informasi_objek_wisata.css" />
+</head>
+<body>
+    <div class="img"></div>
+		<img src="{{url('asset/objectimg.jpg')}}" alt="objectimg.jpg">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1020472.9351397005!2d119.06629890585938!3d2.4327559900304836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3212abcef5b94afb%3A0xe4cf7029473436a9!2sDerawan%20Islands!5e0!3m2!1sen!2sid!4v1713529497397!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</body>
