d189c9ac9abd816a612a35ce291de0698531e7be
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-04-19 22:41:04 +0700

    ID-44 new commit

--- resources/views/informasi_objek_wisata.php
+++ resources/views/informasi_objek_wisata.php
@@ -11,5 +11,19 @@
 <body>
     <div class="img"></div>
 		<img src="{{url('asset/objectimg.jpg')}}" alt="objectimg.jpg">
+	<div class="card-info">
+		<h3 class="title">Kepulauan Derawan</h3>
+		<p>Kalimantan Timur</p>
+		<div class="rating">
+          <i class="fa-solid fa-star"></i>
+          <div class="rating-value">4.8</div>
+          <div class="reviews">85 Reviews</div>
+		  <div class="tickets">1K Tiket Terjual</div>
+        </div>
+			<div class="button-container">
+				<button type="button" class="btn btn-outline-info">Bagikan</button>
+				<button type="button" class="btn btn-outline-info">Tambah ke Wishlist</button>
+			</div>
+	</div>
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1020472.9351397005!2d119.06629890585938!3d2.4327559900304836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3212abcef5b94afb%3A0xe4cf7029473436a9!2sDerawan%20Islands!5e0!3m2!1sen!2sid!4v1713529497397!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </body>
