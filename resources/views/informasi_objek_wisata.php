c774ed9cbbcb6386d98998b29edb5fb72ad56ca7
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-04-20 17:07:16 +0700

    ID-45 new update

--- resources/views/informasi_objek_wisata.php
+++ resources/views/informasi_objek_wisata.php
@@ -55,7 +55,15 @@
 
 		
 	</div>
-	
+	<div class="location-content">
+	<h4>Lokasi</h4>
+	<div class="fs-5 mb-3">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  			<path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+  			<path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+		</svg>
+		PULAU DERAWAN KECAMATAN, Jl. Pulau Derawan, Kabupaten Berau, Kalimantan Timur 77315
+	</div>
 	<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1020472.9351397005!2d119.06629890585938!3d2.4327559900304836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3212abcef5b94afb%3A0xe4cf7029473436a9!2sDerawan%20Islands!5e0!3m2!1sen!2sid!4v1713529497397!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	</div>
 </body>
