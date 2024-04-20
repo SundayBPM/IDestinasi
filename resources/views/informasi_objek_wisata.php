543cc387a8c41a0f6a9885da15576ec86f29a17e
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-04-20 15:24:53 +0700

    ID-44 new update

--- resources/views/informasi_objek_wisata.php
+++ resources/views/informasi_objek_wisata.php
@@ -16,14 +16,32 @@
 		<p>Kalimantan Timur</p>
 		<div class="rating">
           <i class="fa-solid fa-star"></i>
-          <div class="rating-value">4.8</div>
-          <div class="reviews">85 Reviews</div>
-		  <div class="tickets">1K Tiket Terjual</div>
+          		<div class="rating-value">4.8</div>
+          		<div class="reviews">85 Reviews</div>
+			<i class="bi bi-ticket-perforated"></i>	
+		  		<div class="tickets">1K Tiket Terjual</div>
         </div>
 			<div class="button-container">
-				<button type="button" class="btn btn-outline-info">Bagikan</button>
-				<button type="button" class="btn btn-outline-info">Tambah ke Wishlist</button>
+				<button type="button" class="btn btn-outline-info">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
+  					<path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1z"/>
+  					<path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708z"/>
+					</svg>
+					Bagikan
+				</button>
+				<button type="button" class="btn btn-outline-info">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+  					<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+					</svg>
+					Tambah ke Wishlist
+				</button>
 			</div>
+	</div>
+	<div class="date-content">
+		<h4>Jam Operasional</h4>
+		<i class="bi bi-clock"></i>
+		<p style="color:orange">Buka</p>
+		<p>Senin - Minggu pukul 12:00 -19.00 WIB</p>
 	</div>
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1020472.9351397005!2d119.06629890585938!3d2.4327559900304836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3212abcef5b94afb%3A0xe4cf7029473436a9!2sDerawan%20Islands!5e0!3m2!1sen!2sid!4v1713529497397!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </body>
