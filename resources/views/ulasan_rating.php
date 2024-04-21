f8725897755d4966c7a7b8b4ace6148a75cd723a
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-04-21 16:20:30 +0700

    ID-46 update commit

---
+++ resources/views/ulasan_rating.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Informasi Objek Wisata iDestinasi</title>
+  	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/ulasan_ratingcss"/>
+</head>
+<body>
+    <div class="container">
+		<h4>Ulasan dan Rating</h4>
+		<p>Lihat rating dan ulasan yang dikirim dari pengunjung lain</p>
+	<div class="u-container-layout u-similar-container u-container-layout-3">
+        <h5 class="u-text u-text-8">Kirana</h5>
+			<i class="fa-solid fa-star"></i>
+          		<div class="rating-value">4.8</div>
+                  <p>21 Maret 2024, 19:45 WIB</p>
+			<p class="u-text u-text-10">
+				Bagus banget pantainya, fasilitasnya juga
+				mendukung. Sangat recommended untuk
+				teman-teman berkunjung ketika liburan!
+			</p>
+	</div>
+	<div class="u-container-layout u-similar-container u-container-layout-3">
+        <h5 class="u-text u-text-8">Kirana</h5>
+			<i class="fa-solid fa-star"></i>
+          		<div class="rating-value">4.8</div>
+                  <p>21 Maret 2024, 19:45 WIB</p>
+			<p class="u-text u-text-10">
+				Bagus banget pantainya, fasilitasnya juga
+				mendukung. Sangat recommended untuk
+				teman-teman berkunjung ketika liburan!
+			</p>
+	</div>
+	<div class="u-container-layout u-similar-container u-container-layout-3">
+        <h5 class="u-text u-text-8">Kirana</h5>
+			<i class="fa-solid fa-star"></i>
+          		<div class="rating-value">4.8</div>
+                  <p>21 Maret 2024, 19:45 WIB</p>
+			<p class="u-text u-text-10">
+				Bagus banget pantainya, fasilitasnya juga
+				mendukung. Sangat recommended untuk
+				teman-teman berkunjung ketika liburan!
+			</p>
+	</div>
