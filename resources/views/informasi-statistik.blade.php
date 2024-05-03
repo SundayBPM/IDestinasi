230e22a24ce0bf138274f1f0f806204cd1e83570
SundayBPM <sundaybontong@gmail.com>
2024-05-03 16:48:25 +0700

    ID-54 ID-55 ID-56 ID-58 Menyelesaikan beberapa sub fitur dari fitur informasi statistik objek wisata

--- resources/views/informasi-statistik.blade.php
+++ resources/views/informasi-statistik.blade.php
@@ -13,7 +13,7 @@
                 <div class="item_box">
                     <i class="bi bi-people"></i>
                 </div>
-                <h2>12,499</h2>
+                <h2>{{ $total_kunjugangan }}</h2>
                 <p>Total Pengunjung Aktif</p>
                 <p><i class="bi bi-arrow-up-right"></i>  +10% Minggu ini</p>
             </div>
@@ -22,7 +22,7 @@
                 <div class="item_box">
                     <i class="bi bi-backpack"></i>
                 </div>
-                <h2>4,590</h2>
+                <h2>{{ $pembelianTikets }}</h2>
                 <p>Total Pembelian Tiket</p>
                 <p><i class="bi bi-arrow-up-right"></i>  +0,49% Minggu ini</p>                
             </div>
@@ -31,7 +31,7 @@
                 <div class="item_box">
                     <i class="bi bi-check2-circle"></i>
                 </div>
-                <h2>IDR 4,560,000.00</h2>
+                <h2>IDR {{ $total_keuntungan }}</h2>
                 <p>Keuntungan</p>
                 <p><i class="bi bi-arrow-down-left"></i>  +0,25% Minggu ini</p>                
             </div>
@@ -43,10 +43,6 @@
                 <h5 class="category">Tiket Terjual</h5>
                 <h5 class="monthly">monthly</h5>
             </div>
-
-            {!! $objekWisataChart->container() !!}
-            <script src="{{ $objekWisataChart ->cdn() }}"></script>
-            {{ $chart->script() }}
             
             <div class="custom_chart">
                 <canvas id="sales-revenue"></canvas>
@@ -60,6 +56,32 @@
                 <!-- <h1>TESTING</h1> -->
                 <canvas id="nps-rating"></canvas>
             </div>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script>
+                const ctx = document.getElementById('sales-revenue');
+                const labels = <?php echo json_encode($datas['labels']) ?>;
+
+                new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: '# of Votes',
+                            data: <?php echo json_encode($datas['data']) ?>,
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                            beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            
+            </script>            
+
             <div class="others_rating">
                 <div class="row_rating">
                     <h1>Penilaian</h1>
@@ -67,19 +89,19 @@
 
                 <div class="row_rating">
                     <div class="rate">
-                        <h2>4,5</h2>
+                        <h2>{{ $overalRating }}</h2>
                         <h4>Overal Rating From Customer</h4>
                         <h6>3900 Responden</h6>
                     </div>
     
                     <div class="rate">
-                        <h2>7,9</h2>
+                        <h2>{{ $amenitas_rating }}</h2>
                         <h4>Rating Amenitas</h4>
                         <h6>3900 Responden</h6>
                     </div>
     
                     <div class="rate">
-                        <h2>6,5</h2>
+                        <h2>{{ $ansilari1_rating }}</h2>
                         <h4>Rating Ansilari</h4>
                         <h6>3900 Responden</h6>
                     </div>
@@ -87,19 +109,19 @@
 
                 <div class="row_rating">
                     <div class="rate">
-                        <h2>6,7</h2>
+                        <h2>{{ $ansilari2_rating }}</h2>
                         <h4>Rating Ansilari</h4>
                         <h6>3900 Responden</h6>
                     </div>
     
                     <div class="rate">
-                        <h2>9,3</h2>
+                        <h2>{{ $aksesibilitas_rating }}</h2>
                         <h4>Rating Aksesibilitas</h4>
                         <h6>3900 Responden</h6>
                     </div>
     
                     <div class="rate">
-                        <h2>6,7</h2>
+                        <h2>{{ $atraksi_rating }}</h2>
                         <h4>Rating Atraksi</h4>
                         <h6>3900 Responden</h6>
                     </div>
