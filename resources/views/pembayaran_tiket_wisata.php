ebc716ee50f6b27d354352f634a697f91a6ae193
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-05-07 19:47:09 +0700

    new push ID-62 ID-63 ID-64

---
+++ resources/views/pembayaran_tiket_wisata.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Ulasan dan Rating​, ​Lihat rating dan ulasan yang dikirim oleh pengunjung lain, Kirana, 4.8, Kirana, 4.8">
+    <meta name="description" content="">
+    <title>Home</title>
+    <link rel="stylesheet" href="pembayaran_tiket_wisata.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <meta name="referrer" content="origin">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=DM+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": ""
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-home-page="Home.html" data-home-page-title="Home" data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="en">
+    <section class="u-align-center u-clearfix u-section-1" id="carousel_558e">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="custom-expanded u-container-style u-grey-5 u-group u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <div class="custom-expanded u-align-left u-container-style u-group u-radius u-shape-round u-white u-group-2">
+              <div class="u-container-layout u-container-layout-2">
+                <h2 class="u-custom-font u-text u-text-default u-text-1">Informasi Pribadi</h2>
+                <div class="u-border-1 u-border-custom-color-8 u-expanded-width u-line u-line-horizontal u-line-1"></div>
+                <h2 class="u-custom-font u-text u-text-default u-text-2">Wisatawan 1 </h2>
+                <div class="custom-expanded u-align-left u-form u-form-1">
+                  <form action="https://forms.nicepagesrv.com/v2/form/process" class="u-clearfix u-form-spacing-25 u-form-vertical u-inner-form" style="padding: 0px;" source="email" name="form">
+                    <div class="u-form-group u-form-name u-label-none u-form-group-1">
+                      <label for="name-5a14" class="u-custom-font u-form-control-hidden u-label u-label-1" wfd-invisible="true">Name</label>
+                      <input type="text" placeholder="Nama lengkap" id="name-5a14" name="name" class="u-border-custom-color-8 u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-none" required="">
+                    </div>
+                    <div class="u-form-email u-form-group u-label-none">
+                      <label for="email-5a14" class="u-custom-font u-form-control-hidden u-label u-label-2" wfd-invisible="true">Email</label>
+                      <input type="email" placeholder="Email" id="email-5a14" name="email" class="u-border-custom-color-8 u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-none" required="">
+                    </div>
+                    <div class="u-form-group u-form-phone u-label-none u-form-group-3">
+                      <label for="phone-48ed" class="u-custom-font u-form-control-hidden u-label u-label-3"></label>
+                      <input type="tel" placeholder="Nomor Telepon" id="phone-48ed" name="phone" class="u-border-custom-color-8 u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-none" required="" data-country-code="id">
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div class="u-radius u-shape u-shape-round u-white u-shape-1"></div>
+            <img class="u-image u-image-round u-radius u-image-1" src="img/derawan.jpg" alt="" data-image-width="850" data-image-height="638">
+            <h2 class="u-text u-text-3"> Kepulauan Derawan , Kaltim</h2><span class="u-file-icon u-icon u-text-custom-color-9 u-icon-1"><img src="img/2099045-fb9ddd46.png" alt=""></span>
+            <h5 class="u-custom-font u-text u-text-black u-text-default u-text-4">4.8<br>
+            </h5>
+            <h2 class="u-custom-font u-text u-text-5"> Rp 100.000/pax</h2>
+            <h5 class="u-custom-font u-text u-text-custom-color-10 u-text-6"> 85 Reviews</h5>
+            <div class="custom-expanded u-align-left u-container-style u-group u-radius u-shape-round u-white u-group-3">
+              <div class="u-container-layout u-container-layout-3">
+                <h2 class="u-custom-font u-text u-text-default u-text-7">Opsi Pembayaran</h2>
+                <div class="u-border-1 u-border-custom-color-8 u-expanded-width u-line u-line-horizontal u-line-2"></div>
+                <h2 class="u-custom-font u-text u-text-default u-text-8">Metode Pembayaran</h2>
+                <div class="u-border-1 u-border-custom-color-8 u-radius u-shape u-shape-round u-shape-2"></div>
+                <img class="u-image u-image-default u-preserve-proportions u-image-2" src="img/qriis.jpg" alt="" data-image-width="58" data-image-height="63">
+                <div class="u-border-1 u-border-custom-color-8 u-radius u-shape u-shape-round u-shape-3"></div>
+                <img class="u-image u-image-default u-preserve-proportions u-image-3" src="img/atmbersama.png" alt="" data-image-width="68" data-image-height="31">
+                <div class="u-form u-form-radios-spacing-0 u-form-2">
+                  <form action="https://forms.nicepagesrv.com/v2/form/process" class="u-clearfix u-form-horizontal u-form-spacing-0 u-inner-form" source="email" name="method-radios" style="padding: 0px;">
+                    <div class="u-form-group u-form-radiobutton u-label-none u-form-group-5">
+                      <label class="u-label">Radios</label>
+                      <div class="u-form-radio-button-wrapper">
+                        <div class="u-input-row">
+                          <input id="field-7f21" type="radio" name="radiobutton-1" value="MBanking" class="u-field-input" checked="checked" data-calc="">
+                          <label class="u-custom-font u-field-label" for="field-7f21" style="font-family:DM Sans; font-weight: 700;">MBanking</label>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="u-align-left u-form-group u-form-submit u-label-none">
+                      <a href="#" class="u-border-none u-btn u-btn-submit u-button-style u-none u-btn-2"></a>
+                      <input type="submit" value="submit" class="u-form-control-hidden">
+                    </div>
+                    <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+                    <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
+                    <input type="hidden" value="" name="recaptchaResponse">
+                    <input type="hidden" name="formServices" value="dd6099a9-a330-7653-417c-6b96f23adfd0">
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div class="u-form u-form-3">
+              <form action="https://forms.nicepagesrv.com/v2/form/process" class="u-clearfix u-form-horizontal u-form-spacing-10 u-inner-form" source="email" name="form-1" style="padding: 10px;">
+                <div class="u-form-group u-form-radiobutton u-label-none u-form-group-7">
+                  <label class="u-label">Radios</label>
+                  <div class="u-form-radio-button-wrapper">
+                    <div class="u-input-row">
+                      <input id="field-8664" type="radio" name="radiobutton" value="Qris" class="u-field-input" data-calc="">
+                      <label class="u-custom-font u-field-label" for="field-8664" style="font-family:DM Sans; font-weight: 700;">Qris</label>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <h2 class="u-custom-font u-text u-text-default u-text-9"> Reservasi Tiket</h2>
+        <h2 class="u-custom-font u-text u-text-custom-color-2 u-text-default u-text-10">Isi informasi dibawah ini untuk melanjutkan pembelian tiket&nbsp;</h2>
+        <a href="#" class="u-border-1 u-border-active-custom-color-11 u-border-custom-color-8 u-border-hover-custom-color-8 u-btn u-btn-round u-button-style u-none u-radius u-btn-4"><span class="u-file-icon u-icon u-text-black u-icon-2"><img src="img/10136720-ed3aa50f.png" alt=""></span>
+        </a>
+      </div>
+    </section>
+</body>
+</html>
