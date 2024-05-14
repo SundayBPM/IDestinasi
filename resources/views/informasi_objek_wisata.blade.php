f18a0bb1f5a18d18db1dd778cc6d187d26e26a2b
ziyanhaitan <ziyanhaitan@student.telkomuniversity.com>
2024-05-14 21:25:22 +0700

    update commit push ID-44

---
+++ resources/views/informasi_objek_wisata.blade.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Reservasi Tiket">
+    <meta name="description" content="">
+    <title>informasi objek wisata</title>
+	<link rel="stylesheet" href="informasi_objek_wisata.css" media="screen">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=DM+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="informasi objek wisata">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="en">
+    <section class="u-clearfix u-section-1" id="sec-991d">
+      <img class="u-expanded-width u-image u-image-default u-image-1" src="img/derawan.jpg" alt="" data-image-width="850" data-image-height="638">
+      <a href="#" class="u-border-1 u-border-custom-color-8 u-btn u-btn-round u-button-style u-hover-white u-radius u-text-body-alt-color u-white u-btn-1"><span class="u-file-icon u-icon u-text-black u-icon-1"><img src="img/10136720-ed3aa50f.png" alt=""></span>
+      </a>
+      <h1 class="u-custom-font u-text u-text-default u-text-1">Kepuluan Derawan</h1>
+      <h2 class="u-custom-font u-text u-text-custom-color-8 u-text-2">Kalimantan Timur</h2>
+      <div class="u-clearfix u-group-elements u-group-elements-1">
+        <a href="#" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-12 u-custom-font u-radius u-text-custom-color-9 u-btn-2">Terjangkau </a>
+        <a href="#" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-12 u-custom-font u-radius u-text-custom-color-9 u-btn-3">Pantai </a>
+      </div>
+      <div class="u-align-center u-container-style u-group u-opacity u-opacity-0 u-shape-rectangle u-group-1">
+        <div class="u-container-layout">
+          <div class="u-absolute-hcenter u-border-1 u-border-grey-dark-1 u-line u-line-vertical u-line-1"></div>
+          <p class="u-custom-font u-text u-text-custom-color-13 u-text-3">Tiket Terjual </p>
+          <p class="u-custom-font u-text u-text-custom-color-13 u-text-4">85 Reviews </p>
+          <p class="u-custom-font u-text u-text-default u-text-5">1K </p>
+          <p class="u-custom-font u-text u-text-default u-text-6">4.8<br>
+          </p>
+          <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-2" src="img/2099045-fb9ddd46.png" alt="" data-image-width="128" data-image-height="128">
+          <div class="u-rotation-parent u-rotation-parent-1"><span class="u-file-icon u-icon u-icon-2"><img src="img/7411157.png" alt=""></span>
+          </div>
+        </div>
+      </div>
+      <div class="u-clearfix u-group-elements u-group-elements-2">
+        <a href="#" class="u-border-1 u-border-custom-color-11 u-btn u-btn-round u-button-style u-custom-font u-radius u-text-custom-color-11 u-white u-btn-4"><span class="u-file-icon u-icon u-text-custom-color-11 u-icon-3"><img src="img/5381808-2d2767e5.png" alt=""></span>Tambah ke Wishlist<br>
+        </a>
+        <a href="#" class="u-border-1 u-border-custom-color-11 u-btn u-btn-round u-button-style u-custom-font u-radius u-text-custom-color-11 u-white u-btn-5"><span class="u-file-icon u-icon u-text-custom-color-11 u-icon-4"><img src="img/9121736-b346027a.png" alt=""></span>&nbsp;Bagikan
+        </a>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-2" id="sec-8eb2">
+      <div class="u-border-20 u-border-grey-5 u-expanded-width u-line u-line-horizontal u-line-1"></div>
+      <h2 class="u-custom-font u-text u-text-1">Jam Operasional<br>
+      </h2>
+      <p class="u-custom-font u-text u-text-custom-color-9 u-text-2">Buk​a</p>
+      <p class="u-custom-font u-text u-text-custom-color-8 u-text-3"> Senin - Minggu pukul 12:00 -19.00 WIB</p><span class="u-file-icon u-icon u-text-custom-color-8 u-icon-1"><img src="img/10068341-e973da87.png" alt=""></span>
+      <h2 class="u-custom-font u-text u-text-4">Deskripsi<br>
+      </h2>
+      <p class="u-align-justify u-custom-font u-text u-text-custom-color-8 u-text-5"> Salah satu daya tarik utama Kepulauan Derawan adalah Terumbu Karangnya yang masih terjaga dengan baik. Terumbu karang yang indah ini menjadi rumah bagi berbagai jenis biota laut, seperti ikan-ikan warna-warni, penyu, dan ubur-ubur. Selain itu, Kepulauan Derawan juga terkenal dengan wisata hiu pausnya, di mana wisatawan dapat menyaksikan hiu paus dari dekat. Selain keindahan bawah lautnya, Kepulauan Derawan juga memiliki pantai pasir putih yang menawan dan air laut yang jernih. Pulau-pulau di sekitarnya, seperti Pulau Maratua dan Pulau Kakaban, juga menawarkan pesona alam yang tidak kalah menariknya.</p>
+      <h2 class="u-custom-font u-text u-text-6">Foto Objek Wisata<br>
+      </h2>
+      <div class="u-gallery u-layout-grid u-lightbox u-no-transition u-show-text-none u-gallery-1">
+        <div class="u-gallery-inner u-gallery-inner-1">
+          <div class="u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
+            <div class="u-back-slide">
+              <img class="u-back-image u-expanded" src="img/object1.jpg" data-image-width="368" data-image-height="368">
+            </div>
+            <div class="u-over-slide u-shading u-over-slide-1"></div>
+          </div>
+          <div class="u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
+            <div class="u-back-slide">
+              <img class="u-back-image u-expanded" src="img/object2.jpg" data-image-width="368" data-image-height="368">
+            </div>
+            <div class="u-over-slide u-shading u-over-slide-2"></div>
+          </div>
+          <div class="u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
+            <div class="u-back-slide">
+              <img class="u-back-image u-expanded" src="img/object3.jpg" data-image-width="368" data-image-height="368">
+            </div>
+            <div class="u-over-slide u-shading u-over-slide-3"></div>
+          </div>
+          <div class="u-effect-hover-zoom u-gallery-item u-shape-rectangle" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
+            <div class="u-back-slide">
+              <img class="u-back-image u-expanded" src="img/object4.jpg" data-image-width="368" data-image-height="368">
+            </div>
+            <div class="u-over-slide u-shading u-over-slide-4"></div>
+          </div>
+        </div>
+      </div>
+      <h2 class="u-custom-font u-text u-text-7">Lokasi<br>
+      </h2>
+      <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-1" src="img/location.jpg" alt="" data-image-width="34" data-image-height="40">
+      <p class="u-custom-font u-text u-text-8"> PULAU DERAWAN KECAMATAN, Jl. Pulau Derawan, Kabupaten Berau, Kalimantan Timur 77315</p>
+      <div class="u-grey-light-2 u-map u-map-1">
+        <div class="embed-responsive">
+          <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510311.75962579943!2d118.46342415000001!3d2.22487435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3212abcef5b94afb%3A0xe4cf7029473436a9!2sDerawan%20Islands!5e0!3m2!1sen!2sid!4v1715347841954!5m2!1sen!2sid" data-map="JTdCJTIycG9zaXRpb25UeXBlJTIyJTNBJTIybWFwLWVtYmVkJTIyJTJDJTIyYWRkcmVzcyUyMiUzQSUyMk1hbmhhdHRhbiUyQyUyME5ldyUyMFlvcmslMjIlMkMlMjJ6b29tJTIyJTNBMTAlMkMlMjJ0eXBlSWQlMjIlM0ElMjJyb2FkJTIyJTJDJTIybGFuZyUyMiUzQW51bGwlMkMlMjJhcGlLZXklMjIlM0FudWxsJTJDJTIybWFya2VycyUyMiUzQSU1QiU1RCUyQyUyMmVtYmVkJTIyJTNBJTIyaHR0cHMlM0ElMkYlMkZ3d3cuZ29vZ2xlLmNvbSUyRm1hcHMlMkZlbWJlZCUzRnBiJTNEITFtMTghMW0xMiExbTMhMWQ1MTAzMTEuNzU5NjI1Nzk5NDMhMmQxMTguNDYzNDI0MTUwMDAwMDEhM2QyLjIyNDg3NDM1ITJtMyExZjAhMmYwITNmMCEzbTIhMWkxMDI0ITJpNzY4ITRmMTMuMSEzbTMhMW0yITFzMHgzMjEyYWJjZWY1Yjk0YWZiJTI1M0EweGU0Y2Y3MDI5NDczNDM2YTkhMnNEZXJhd2FuJTI1MjBJc2xhbmRzITVlMCEzbTIhMXNlbiEyc2lkITR2MTcxNTM0Nzg0MTk1NCE1bTIhMXNlbiEyc2lkJTIyJTdE"></iframe>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-3" id="sec-09ed">
+      <div class="u-border-20 u-border-grey-5 u-expanded-width u-line u-line-horizontal u-line-1"></div>
+      <h2 class="u-custom-font u-text u-text-1">Ulasan dan Rating </h2>
+      <p class="u-custom-font u-text u-text-custom-color-8 u-text-2"> Lihat rating dan ulasan yang dikirim oleh pengunjung lain</p>
+      <a href="#" class="u-border-1 u-border-custom-color-11 u-btn u-btn-round u-button-style u-custom-font u-radius u-text-custom-color-11 u-white u-btn-1">Lihat Semua Ulasan </a>
+      <div class="u-layout-horizontal u-list u-list-1">
+        <div class="u-repeater u-repeater-1">
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-1">
+              <div class="u-border-1 u-border-custom-color-14 u-custom-item u-radius u-shape u-shape-round u-shape-1"></div>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-1" src="img/womanprofilepic.png" alt="" data-image-width="83" data-image-height="83">
+              <p class="u-custom-font u-custom-item u-text u-text-3">Nazwa Kirana</p>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-2" src="img/2099045-fb9ddd46.png" alt="" data-image-width="128" data-image-height="128">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-4">4.8<br>
+              </p>
+              <p class="u-custom-font u-custom-item u-text u-text-5"> Bagus banget pantainya, fasilitasnya juga mendukung. Sangat recommended untuk teman-teman berkunjung ketika  liburan!</p>
+            </div>
+          </div>
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-2">
+              <div class="u-border-1 u-border-custom-color-14 u-custom-item u-radius u-shape u-shape-round u-shape-2"></div>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-3" src="img/womanprofilepic.png" alt="" data-image-width="83" data-image-height="83">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-6">Nazwa Kirana</p>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-4" src="img/2099045-fb9ddd46.png" alt="" data-image-width="128" data-image-height="128">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-7">4.8<br>
+              </p>
+              <p class="u-custom-font u-custom-item u-text u-text-8"> Bagus banget pantainya, fasilitasnya juga mendukung. Sangat recommended untuk teman-teman berkunjung ketika  liburan!</p>
+            </div>
+          </div>
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-3">
+              <div class="u-border-1 u-border-custom-color-14 u-custom-item u-radius u-shape u-shape-round u-shape-3"></div>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-5" src="img/womanprofilepic.png" alt="" data-image-width="83" data-image-height="83">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-9">Nazwa Kirana</p>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-6" src="img/2099045-fb9ddd46.png" alt="" data-image-width="128" data-image-height="128">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-10">4.8<br>
+              </p>
+              <p class="u-custom-font u-custom-item u-text u-text-11"> Bagus banget pantainya, fasilitasnya juga mendukung. Sangat recommended untuk teman-teman berkunjung ketika  liburan!</p>
+            </div>
+          </div>
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-4">
+              <div class="u-border-1 u-border-custom-color-14 u-custom-item u-radius u-shape u-shape-round u-shape-4"></div>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-7" src="img/womanprofilepic.png" alt="" data-image-width="83" data-image-height="83">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-12">Nazwa Kirana</p>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-8" src="img/2099045-fb9ddd46.png" alt="" data-image-width="128" data-image-height="128">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-13">4.8<br>
+              </p>
+              <p class="u-custom-font u-custom-item u-text u-text-14"> Bagus banget pantainya, fasilitasnya juga mendukung. Sangat recommended untuk teman-teman berkunjung ketika  liburan!</p>
+            </div>
+          </div>
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-5">
+              <div class="u-border-1 u-border-custom-color-14 u-custom-item u-radius u-shape u-shape-round u-shape-5"></div>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-9" src="img/womanprofilepic.png" alt="" data-image-width="83" data-image-height="83">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-15">Nazwa Kirana</p>
+              <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-10" src="img/2099045-fb9ddd46.png" alt="" data-image-width="128" data-image-height="128">
+              <p class="u-custom-font u-custom-item u-text u-text-default u-text-16">4.8<br>
+              </p>
+              <p class="u-custom-font u-custom-item u-text u-text-17"> Bagus banget pantainya, fasilitasnya juga mendukung. Sangat recommended untuk teman-teman berkunjung ketika  liburan!</p>
+            </div>
+          </div>
+        </div>
+        <a class="u-gallery-nav u-gallery-nav-prev u-grey-25 u-hidden u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-1" href="#" role="button">
+          <span aria-hidden="true">
+            <svg viewBox="0 0 451.847 451.847"><path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path></svg>
+          </span>
+          <span class="sr-only">
+            <svg viewBox="0 0 451.847 451.847"><path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"></path></svg>
+          </span>
+        </a>
+        <a class="u-gallery-nav u-gallery-nav-next u-grey-25 u-hidden u-icon-circle u-opacity u-opacity-70 u-spacing-10 u-text-white u-gallery-nav-2" href="#" role="button">
+          <span aria-hidden="true">
+            <svg viewBox="0 0 451.846 451.847"><path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path></svg>
+          </span>
+          <span class="sr-only">
+            <svg viewBox="0 0 451.846 451.847"><path d="M345.441,248.292L151.154,442.573c-12.359,12.365-32.397,12.365-44.75,0c-12.354-12.354-12.354-32.391,0-44.744
+L278.318,225.92L106.409,54.017c-12.354-12.359-12.354-32.394,0-44.748c12.354-12.359,32.391-12.359,44.75,0l194.287,194.284
+c6.177,6.18,9.262,14.271,9.262,22.366C354.708,234.018,351.617,242.115,345.441,248.292z"></path></svg>
+          </span>
+        </a>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-4" id="sec-fd00">
+      <div class="u-border-20 u-border-grey-5 u-expanded-width u-line u-line-horizontal u-line-1"></div>
+      <div class="u-clearfix u-group-elements u-group-elements-1">
+        <h2 class="u-custom-font u-text u-text-default u-text-1">Pilih Tanggal</h2>
+        <p class="u-custom-font u-text u-text-custom-color-8 u-text-2">Silahkan pilih tanggal yang tersedia</p>
+      </div>
+      <div class="u-clearfix u-group-elements u-group-elements-2">
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-1" rel="nofollow"> 18 Maret<br>
+          <span style="font-weight: 400;">Senin</span>
+        </a>
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-2" rel="nofollow"> 19 Maret<br>
+          <span style="font-weight: 400;">Selasa</span>
+        </a>
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-3" rel="nofollow"> 20 Maret<br>
+          <span style="font-weight: 400;">Rabu</span>
+        </a>
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-4" rel="nofollow"> 21 Maret<br>
+          <span style="font-weight: 400;">Kamis</span>
+        </a>
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-5" rel="nofollow"> 22 Maret<br>
+          <span style="font-weight: 400;">Jumat</span>
+        </a>
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-6" rel="nofollow"> 23 Maret<br>
+          <span style="font-weight: 400;">Sabtu</span>
+        </a>
+        <a href="#" class="u-btn u-btn-round u-button-style u-custom-color-14 u-custom-font u-custom-item u-hover-custom-color-11 u-radius u-text-custom-color-15 u-text-hover-white u-btn-7" rel="nofollow"> 24 Maret<br>
+          <span style="font-weight: 400;">Minggu</span>
+        </a>
+      </div>
+    </section>
+
+</body>
+</html>
