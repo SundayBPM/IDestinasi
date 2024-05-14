8c332781b0e5c7918d2c69a3a90399e8030cfcf2
Nazwa Tazkia Kirana <>
2024-05-14 23:26:03 +0700

    ID-51 Informasi detail paket tour

---
+++ database/migrations/2024_05_12_184911_create_objek_wisata_fasilitas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('objek_wisata_fasilitas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('objek_wisata_id');
+            $table->string('fasilitas');
+            $table->timestamps();
+
+            $table->foreign('objek_wisata_id')->references('id')->on('artikel')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('objek_wisata_fasilitas');
+    }
+};
